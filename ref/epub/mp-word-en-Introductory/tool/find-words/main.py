f633e108172ccc220329719ad0dab0d8059408d0
kei <wogud86@gmail.com>
2025-10-04 15:35:22 +0900

    feat: add find-words tool

---
+++ ref/epub/mp-word-en-Introductory/tool/find-words/main.py
@@ -0,0 +1,20 @@
+
+def remove_already_words():
+    with open("need.txt", 'r') as f:
+        needs = [ line for line in f.readlines()]
+
+    with open("need.txt", 'w') as f:
+        for need in needs:
+            f.write(need)
+
+
+
+def run():
+    print()
+
+
+# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+if __name__ == '__main__':
+    remove_already_words()
+    # run()
+    pass
