120aa881eec720013687bddd664632b7f943a567
kei <wogud86@gmail.com>
2025-10-04 15:37:02 +0900

    feat: 이미 있는 단어 삭제

--- ref/epub/mp-word-en-Introductory/tool/find-words/main.py
+++ ref/epub/mp-word-en-Introductory/tool/find-words/main.py
@@ -1,13 +1,16 @@
 
 def remove_already_words():
+    with open("already.txt", 'r') as f:
+        already_list = [line for line in f.readlines()]
+
     with open("need.txt", 'r') as f:
         needs = [ line for line in f.readlines()]
+
+    needs = [need for need in needs if need not in already_list]
 
     with open("need.txt", 'w') as f:
         for need in needs:
             f.write(need)
-
-
 
 def run():
     print()
