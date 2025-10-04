68889c5bbfb96216db430998741f0a550750a0fb
kei <wogud86@gmail.com>
2025-10-04 16:02:03 +0900

    feat: 필요없을 것 같은 단어 삭제

--- ref/epub/mp-word-en-Introductory/tool/find-words/main.py
+++ ref/epub/mp-word-en-Introductory/tool/find-words/main.py
@@ -1,16 +1,24 @@
+import os
 
 def remove_already_words():
     with open("already.txt", 'r') as f:
-        already_list = [line for line in f.readlines()]
+        already_list = [line.rstrip('\n') for line in f.readlines()]
 
     with open("need.txt", 'r') as f:
-        needs = [ line for line in f.readlines()]
+        needs = [ line.rstrip('\n') for line in f.readlines()]
 
     needs = [need for need in needs if need not in already_list]
 
     with open("need.txt", 'w') as f:
-        for need in needs:
-            f.write(need)
+        f.write('\n'.join(needs))
+
+def make_already_words():
+    path = r'C:\Users\sojun\github\mp-word\assets\images\words'
+    items = os.listdir(path)
+    asset_words = [name for name in items if os.path.isdir(os.path.join(path, name))]
+    print(asset_words)
+    with open("already.txt", 'w') as f:
+        f.write('\n'.join(asset_words))
 
 def run():
     print()
@@ -19,5 +27,6 @@ def run():
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
     remove_already_words()
+    # make_already_words()
     # run()
     pass
