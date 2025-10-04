2579a64ec55eeed3732921cd929f613f3748f5f9
kei <wogud86@gmail.com>
2025-10-04 16:02:19 +0900

    feat: print_gpt_prompts 만듦

--- ref/epub/mp-word-en-Introductory/tool/find-words/main.py
+++ ref/epub/mp-word-en-Introductory/tool/find-words/main.py
@@ -20,13 +20,22 @@ def make_already_words():
     with open("already.txt", 'w') as f:
         f.write('\n'.join(asset_words))
 
-def run():
-    print()
+def print_gpt_prompts():
+
+    with open("need.txt", 'r') as f:
+        needs = [ line.rstrip('\n') for line in f.readlines()[:6*100]]
+
+    need_chunks = [needs[i:i + 6] for i in range(0, len(needs), 6)]
+    for need_chunk in need_chunks:
+        print('Create a single image divided into six equal-sized sections, ensuring each section has the same width and height, and illustrate one of the following words in minimalist line art:')
+        for i in range(len(need_chunk)):
+            print(f'{i+1}. {need_chunk[i]}')
+        print('\n')
 
 
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
-    remove_already_words()
+    # remove_already_words()
     # make_already_words()
-    # run()
+    print_gpt_prompts()
     pass
