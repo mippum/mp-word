48c272de64365a7d5ca51dd7d37480820a50c3b0
kei <wogud86@gmail.com>
2025-10-14 02:23:27 +0900

    feat: add gen_sql

--- ref/tool/gpt_pronunce_repr_py/gen_prompt.py
+++ ref/tool/gpt_pronunce_repr_py/gen_prompt.py
@@ -5,7 +5,8 @@ def run():
     with open('origin_words.txt', 'r', encoding='utf-8') as f:
         origin_words = [line.strip() for line in f.readlines()]
 
-    words_chunks =chunked(origin_words, 50)
+    # words_chunks =chunked(origin_words, 50)
+    words_chunks =chunked(origin_words, 20)
 
     for words_chunk in words_chunks:
         print('이 단어들도 해줘.')
