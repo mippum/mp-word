6970b779546493639e458071a18578b6cdc9f072
kei <wogud86@gmail.com>
2025-10-20 12:56:07 +0900

    fix

--- ref/tool/check_work_contain_py/gen_prompt.py
+++ ref/tool/check_work_contain_py/gen_prompt.py
@@ -6,7 +6,7 @@ def run():
         needs = [ line.rstrip('\n') for line in f.readlines()]
 
     need_chunks = [needs[i:i + 6] for i in range(0, len(needs), 6)]
-    need_chunks = need_chunks[100*1:100*2]
+    # need_chunks = need_chunks[100*1:100*2]
     for need_chunk in need_chunks:
         print('Create a single image divided into six equal-sized sections, ensuring each section has the same width and height, and illustrate one of the following words in minimalist line art:')
         for i in range(len(need_chunk)):
