5bb6e900a5858a2d7635b40aceb7c28ffa505f47
kei <wogud86@gmail.com>
2026-02-13 00:41:38 +0900

    feat: word_icon.svg 를 word_shape_icon.svg 로 바꿈

--- ref/tool/word_csv_data_mng_py/scripts/mng_svg_asset.py
+++ ref/tool/word_csv_data_mng_py/scripts/mng_svg_asset.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import uuid6
+import shutil
 
 # greenydot_word_path = r'C:\Users\USER\github\greenydot_flight_api\flight-app\public\static\mp-word\words'
 asset_path = r'C:\Users\wogud\github\mp-word\assets\images\words'
@@ -11,10 +12,20 @@ asset_path = r'C:\Users\wogud\github\mp-word\assets\images\words'
 
 def run():
 
-    for word in os.listdir(asset_path):
-        # print(os.path.join(asset_path, word))
-        os.rename(os.path.join(asset_path, word),
-                  os.path.join(asset_path, '_' + word))
+    for word_folder_name in os.listdir(asset_path):
+
+        # word = word_folder_name[1:]
+
+        if os.path.exists(os.path.join(asset_path, word_folder_name, 'word_shape_icon.svg')):
+            # print('word_shape_icon')
+            shutil.copy(os.path.join(asset_path, word_folder_name, 'word_shape_icon.svg'),
+                        os.path.join(asset_path, word_folder_name, 'word_icon.svg'))
+
+            pass
+
+        # os.rename(os.path.join(asset_path, word),
+        #           os.path.join(asset_path, '_' + word))
+
         pass
     pass
 
